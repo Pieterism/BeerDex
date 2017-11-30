b1e452be19420e7f8d4ccb3985924fb50818c140
Pieterism <pervdh@gmail.com>
2017-11-30 16:48:03 +0100

    no message

--- BeerDex/src/App.js
+++ BeerDex/src/App.js
@@ -1,4 +1,18 @@
 import React from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 import Login from "./components/Login";
-import {Router, Scene} from "react-native-router-flux";
+import { Router, Scene } from "react-native-router-flux";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Scene key="root">
+          <Scene key="login" component={Login} title="Login" />
+        </Scene>
+      </Router>
+    );
+  }
+}
+
+export default App;
