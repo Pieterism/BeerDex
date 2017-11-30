ad0dd245661a6f6677a289b2503f8b10b25986f0
Pieterism <pervdh@gmail.com>
2017-11-30 16:34:21 +0100

    working setup

--- BeerDex/index.js
+++ BeerDex/index.js
@@ -1,4 +1,5 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from "react-native";
+import { AppRegistry } from "react-native";
+import Login from "./src/components/Login";
 
-AppRegistry.registerComponent('BeerDex', () => App);
+AppRegistry.registerComponent("BeerDex", () => Login);
---
+++ BeerDex/src/App.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import Login from "./components/Login";
+import {Router, Scene} from "react-native-router-flux";
