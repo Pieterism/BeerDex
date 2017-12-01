6ad5468a4ade0a20885076c456ab7d1bae5b0180
Pieterism <pervdh@gmail.com>
2017-12-01 14:14:08 +0100

    splash screen done

--- BeerDex/src/components/Splash.js
+++ BeerDex/src/components/Splash.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import Button from 'react-native-button';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import Button from "react-native-button";
 
 export default class Logo extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Image
-          style={{ width: 50, height: 60 }}
-          source={require("../images/logo.png")}
-        />
-        <Text style={styles.logoText}>Welcome to your BeerDex!</Text>
-        
+        <View style={styles.container}>
+          <Image
+            style={{ width: 70, height: 100 }}
+            source={require("../images/logo.png")}
+          />
+          <Text style={styles.logoText}>Welcome to your BeerDex!</Text>
+        </View>
+        <View style={styles.containerButton}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>DRINK EM ALL</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -29,5 +35,25 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#E28830"
   },
-  buttonText: {}
+  button: {
+    backgroundColor: "#E28830",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20
+  },
+  buttonText: {
+    fontFamily: "BoldDrink",
+    marginBottom: 5,
+    marginTop: 5,
+    marginLeft: 40,
+    marginRight: 40,
+    fontSize: 45,
+    color: "#2A374A"
+  },
+  containerButton:{
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 60
+  }
+
 });
