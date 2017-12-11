f430e3aa95efdd7c0245b4705dd9c5563a78adc8
Pieterism <pervdh@gmail.com>
2017-12-11 13:23:45 +0100

    fixed mapMarker

--- BeerDex/src/components/Map.js
+++ BeerDex/src/components/Map.js
@@ -3,10 +3,28 @@ import { StyleSheet, Text, View } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 export default class Map extends Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+      initialPosition: {
+        latitude: 0,
+        longtitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0
+      },
+      markerPosition: {
+        longtitude:0,
+        latitude: 0
+      }
+    }
+  }
+
   render() {
     const { region } = this.props;
 
     return (
+      // Huidige locatie tonen! 
       <View style={styles.container}>
         <MapView
           provider={PROVIDER_GOOGLE}
@@ -14,8 +32,8 @@ export default class Map extends Component {
           region={{
             latitude: 51.053641,
             longitude: 3.723632,
-            latitudeDelta: 0.01,
-            longitudeDelta: 0.01
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.015
           }}
         >
           <MapView.Marker
@@ -24,8 +42,14 @@ export default class Map extends Component {
               latitude: 51.053641,
               longitude: 3.723632,
             }}
-            
-          />
+            //Naam van bier inzetten
+            title = {"Marker"}
+            description = {"Description"}
+            >
+            <View style = {styles.radius}>
+              <View style = {styles.marker}/>
+            </View>
+          </MapView.Marker>
         </MapView>
       </View>
     );
@@ -45,5 +69,25 @@ const styles = StyleSheet.create({
     top: 0,
     bottom: 0,
     position: "absolute"
+  },
+  radius: {
+    height: 50,
+    width: 50,
+    borderRadius: 50/2,
+    overflow : 'hidden',
+    borderWidth: 1,
+    backgroundColor: 'rgba(0,122,255,0.1)' ,
+    borderColor: 'rgba(0,122,255,0.3)' ,    
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  marker: {
+    height: 20,
+    width: 20,
+    borderRadius: 20/2,
+    overflow : 'hidden',
+    borderWidth: 3,
+    backgroundColor: '#007AFF' ,
+    borderColor: 'white'
   }
 });
