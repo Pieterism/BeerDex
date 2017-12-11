90d2aaa4711bc27014904a5b6b123349a9d0047a
Pieterism <pervdh@gmail.com>
2017-12-11 19:30:38 +0100

    fixed current location

--- BeerDex/src/components/Map.js
+++ BeerDex/src/components/Map.js
@@ -1,59 +1,77 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View, Dimensons, Dimensions } from "react-native";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Dimensons,
+  Dimensions
+} from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import MapStyle from './common/MapStyle.json';
+import MapStyle from "./common/MapStyle.json";
 
-const {width, height}= Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
 const ASPECT_RATIO = width / height;
-const LATITUDE = 0;
-const LONGITUDE = 0;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
-
 export default class Map extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      region: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
+      initialPosition: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0
+      },
+      markerPosition: {
+        latitude: 0,
+        longitude: 0,
       }
     };
   }
 
+  watchID: ?number = null;
+
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({
-          region: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA,
-          }
-        });
+        var lat = parseFloat(position.coords.latitude);
+        var long = parseFloat(position.coords.longitude);
+
+        var initialRegion = {
+          latitude: lat,
+          longitude: long,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA
+        };
+
+        this.setState({ initialPosition: initialRegion });
+        this.setState({ markerPosition: initialRegion });
       },
-    (error) => console.log(error.message),
-    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+      error => alert(JSON.stringify(error)),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 100 }
     );
 
-    this.watchID = navigator.geolocation.watchPosition(
-      position => {
-        this.setState({
-          region: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA,
-          }
-        });
-      }
-    );
+    this.watchID = navigator.geolocation.watchPosition(position => {
+      var lat = parseFloat(position.coords.latitude);
+      var long = parseFloat(position.coords.longitude);
+
+      var lastRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      };
+
+      this.setState({ initialPosition: lastRegion });
+      this.setState({ markerPosition: lastRegion });
+    });
   }
 
   componentWillUnmount() {
@@ -62,23 +80,22 @@ export default class Map extends Component {
 
   render() {
     return (
-      // Huidige locatie tonen! 
+      // Huidige locatie tonen!
       <View style={styles.container}>
         <MapView
           provider={PROVIDER_GOOGLE}
-          style={styles.mapview}
+          style={styles.map}
           customMapStyle={MapStyle}
-          showsUserLocation = {true}
-          onRegionChange={region => this.setState({region})}
-          onRegonChangeComplete={ region => this.setState({region})}>
-
+          region={this.state.initialPosition}
+        >
           <MapView.Marker
-            coordinate ={this.state.region}
-            //Naam van bier inzetten op vaste coordinaten. 
-            //title = {"My Position"}
-            >
-            <View style = {styles.radius}>
-              <View style = {styles.marker}/>
+            coordinate={this.state.markerPosition}
+            //Naam van bier inzetten
+            //title = {"Marker"}
+            //description = {"Description"}
+          >
+            <View style={styles.radius}>
+              <View style={styles.marker} />
             </View>
           </MapView.Marker>
         </MapView>
@@ -94,36 +111,31 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center"
   },
-  mapview: {
-    height: '100%',
-    width: '100%'
-  },
   map: {
     right: 0,
     left: 0,
     top: 0,
     bottom: 0,
     position: "absolute"
-  }/* 
+  },
   radius: {
     height: 50,
     width: 50,
-    borderRadius: 50/2,
-    overflow : 'hidden',
+    borderRadius: 50 / 2,
+    overflow: "hidden",
     borderWidth: 1,
-    backgroundColor: 'rgba(0,122,255,0.1)' ,
-    borderColor: 'rgba(0,122,255,0.3)' ,    
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "rgba(0,122,255,0.1)",
+    borderColor: "rgba(0,122,255,0.3)",
+    alignItems: "center",
+    justifyContent: "center"
   },
-  //uitzicht van marker kunnen we bijwerken naar pinpoint ofzo
   marker: {
     height: 20,
     width: 20,
-    borderRadius: 20/2,
-    overflow : 'hidden',
+    borderRadius: 20 / 2,
+    overflow: "hidden",
     borderWidth: 3,
-    backgroundColor: '#007AFF' ,
-    borderColor: 'white'
-  }*/
+    backgroundColor: "#007AFF",
+    borderColor: "white"
+  }
 });
