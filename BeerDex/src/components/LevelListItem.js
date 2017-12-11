247834ae1f27d05f798e308ba13cffe9a242abad
Pieterism <pervdh@gmail.com>
2017-12-11 23:32:29 +0100

    levelIcons added

--- BeerDex/src/components/LevelListItem.js
+++ BeerDex/src/components/LevelListItem.js
@@ -1,73 +1,76 @@
-import React, {Component} from 'react';
-import { Text , TouchableWithoutFeedback, View } from 'react-native';
-import { CardSection } from './common';
-import { Actions } from 'react-native-router-flux';
-import {connect} from 'react-redux';
-import {levelSelected} from './../actions';
-
+import React, { Component } from "react";
+import { Text, TouchableWithoutFeedback, View, Image } from "react-native";
+import { CardSection } from "./common";
+import { Actions } from "react-native-router-flux";
+import { connect } from "react-redux";
+import { levelSelected } from "./../actions";
 
 class LevelListItem extends Component {
+  renderDescription() {
+    if (this.props.level.free) {
+      return (
+        <Text style={styles.descriptionStyle}>
+          {this.props.level.description}
+        </Text>
+      );
+    }
 
-renderDescription() {
-  if (this.props.level.free){
     return (
       <Text style={styles.descriptionStyle}>
-        {this.props.level.description}
+        You still have lots to learn young padawan!
       </Text>
     );
   }
 
-  return (
-    <Text style={styles.descriptionStyle}>
-      You still have lots to learn young padawan!
-    </Text>
-  )
-}
-
-onButtonPress () {
-  if (this.props.level.free){
-    const {level} = this.props
-    this.props.levelSelected(level);
-    Actions.Beers();
+  onButtonPress() {
+    if (this.props.level.free) {
+      const { level } = this.props;
+      this.props.levelSelected(level);
+      Actions.Beers();
+    }
   }
-}
 
-render () {
+  render() {
+    return (
+      <TouchableWithoutFeedback onPress={this.onButtonPress.bind(this)}>
+        <View>
+          <CardSection>
+            <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+              <Text style={styles.titleStyle}>{this.props.level.title}</Text>
+              <Image
+                style={styles.img}
+                source={require("../images/levelIcon2.png")}
+              />
+            </View>
+          </CardSection>
 
-  return (
-    <TouchableWithoutFeedback
-      onPress = {this.onButtonPress.bind(this)}
-    >
-      <View>
-        <CardSection>
-          <Text style={styles.titleStyle}>
-            {this.props.level.title}
-          </Text>
-        </CardSection>
-
-        <CardSection>
-          {this.renderDescription()}
-        </CardSection>
-      </View>
-    </TouchableWithoutFeedback>
-  );
+          <CardSection>{this.renderDescription()}</CardSection>
+        </View>
+      </TouchableWithoutFeedback>
+    );
   }
 }
 
 const styles = {
   titleStyle: {
-    fontSize: 18,
+    fontSize: 30,
+    fontFamily: "Andy Bold",
+    color: "#E28830",
     paddingLeft: 15
   },
   descriptionStyle: {
     fontSize: 12,
     paddingLeft: 35
+  },
+  img: {
+    width: 25,
+    height: 35
   }
 };
 
-const  mapStateToProps = ({beer}) => {
-  const {levels} = beer;
+const mapStateToProps = ({ beer }) => {
+  const { levels } = beer;
   return levels;
 };
 
-export default connect(mapStateToProps, {levelSelected})(LevelListItem);
+export default connect(mapStateToProps, { levelSelected })(LevelListItem);
