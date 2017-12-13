298505a26ae2bf8322d43e04d674b56f6eee5691
Pieterism <pervdh@gmail.com>
2017-12-13 02:54:27 +0100

    button needs adjustments

--- BeerDex/src/components/BeerDetialaTest.js
+++ BeerDex/src/components/BeerDetialaTest.js
@@ -1,26 +1,56 @@
 import React, { Component } from "react";
-import { Text, View, ScrollView, Image } from "react-native";
+import { Text, View, ScrollView, Image, TouchableOpacity } from "react-native";
 import { connect } from "react-redux";
 import { levelSelected } from "./../actions";
 import BeerItem from "./BeerItem.js";
 import Map from "./Map.js";
-import Button from "./common";
+import { Button } from "./common";
+import { sendData, completed, updateData } from "./../actions";
 
 class Beers extends Component {
-  renderBeerDetails() {}
+  onButtonPress() {
+    const { levels, selectedBeer, selectedLevel } = this.props;
+    selectedBeer.completed = true;
+    selectedLevel.complete++;
+    if (selectedLevel.complete >= 6) {
+      levels.levels[selectedLevel.number + 1].free = true;
+    }
+
+    console.log(selectedBeer);
+    console.log(selectedLevel);
+    this.props.completed({ selectedBeer, selectedLevel });
+    this.props.updateData(levels);
+  }
 
   renderButton() {
     const { completed } = this.props.selectedBeer;
 
     if (completed) {
       return (
-        <View>
-          <Text>Hier Moet een map komen kejet</Text>
-          <Text>Hier moet een foto komen!</Text>
+        <View style={styles.container}>
+          <View style={styles.infoContainer}>
+            <View justifyContent="center" alignItems="center">
+              <Text style={styles.infoTitle}>MAP</Text>
+            </View>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <View justifyContent="center" alignItems="center">
+              <Text style={styles.infoTitle}>PICTURE</Text>
+            </View>
+          </View>
         </View>
       );
     }
-    return <Text>Hier moet een button komen</Text>;
+    return (
+      <TouchableOpacity
+        justifyContent="center"
+        alignItems="center"
+        onPress={this.onButtonPress.bind(this)}
+      >
+        <Text style={styles.buttonTitle}>Drink this beer!</Text>
+      </TouchableOpacity>
+    );
   }
 
   render() {
@@ -32,16 +62,17 @@ class Beers extends Component {
       brewery
     } = this.props.selectedBeer;
     return (
-      <View style={{ paddingTop: 50 }}>
+      <View style={{ paddingTop: 50, flex: 1 }}>
         <View>
           <Text style={styles.titleStyle}>{name}</Text>
 
           <Image style={styles.imageStyle} source={{ uri: image }} />
         </View>
-        <View>
-          <Text>Brouwerij: {brewery}</Text>
-          <Text>Alcoholpercentage: {percentage}</Text>
-          <Text>Info: {description}</Text>
+
+        <View style={{ flex: 1 }}>
+          <Text>{brewery}</Text>
+          <Text>{percentage}</Text>
+          <Text>{description}</Text>
           {this.renderButton()}
         </View>
       </View>
@@ -54,7 +85,8 @@ class Beers extends Component {
       completed,
       percentage,
       description,
-      image
+      image,
+      brewery
     } = this.props.selectedBeer;
     return (
       <View style={styles.container}>
@@ -63,6 +95,42 @@ class Beers extends Component {
             <Text style={styles.titleText}>{name}</Text>
             <Image style={styles.imageStyle} source={{ uri: image }} />
           </View>
+
+          <View style={styles.infoContainer}>
+            <View justifyContent="center" alignItems="center">
+              <Text style={styles.infoTitle}>Algemene Info</Text>
+            </View>
+            <View style={styles.rowView}>
+              <Text style={styles.labelText}>Brouwerij:</Text>
+              <Text style={styles.infoText}>{brewery}</Text>
+            </View>
+            <View style={styles.rowView}>
+              <Text style={styles.labelText}>Alcohol %:</Text>
+              <Text style={styles.infoText}>{percentage}%</Text>
+            </View>
+            <View style={styles.rowView}>
+              <Text style={styles.labelText}>Extra: </Text>
+              <Text style={styles.infoText}>{description}</Text>
+            </View>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <View justifyContent="center" alignItems="center">
+              <Text style={styles.infoTitle}>MAP</Text>
+            </View>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <View justifyContent="center" alignItems="center">
+              <Text style={styles.infoTitle}>PICTURE</Text>
+            </View>
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity justifyContent="center" alignItems="center">
+              <Text style={styles.buttonTitle}>Drink this beer!</Text>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
       </View>
     );
@@ -70,8 +138,8 @@ class Beers extends Component {
 }
 
 const mapStateToProps = ({ beer }) => {
-  const { selectedLevel, selectedBeer } = beer;
-  return { selectedLevel, selectedBeer };
+  const { levels, selectedLevel, selectedBeer } = beer;
+  return { levels, selectedLevel, selectedBeer };
 };
 
 const styles = {
@@ -92,35 +160,64 @@ const styles = {
   },
   imageStyle: {
     flex: 1,
-    height:150,
+    height: 150,
     width: 150,
     resizeMode: "contain",
-    backgroundColor: "#fff",
+    backgroundColor: "#fafafa",
     marginTop: 5,
     borderRadius: 20,
+    borderWidth: 3
+  },
+  infoContainer: {
+    backgroundColor: "#2A374A",
+    justifyContent: "flex-start",
+    marginTop: 10,
+    borderRadius: 20,
     borderWidth: 3,
-    borderColor: "#E28830"
+    borderColor: "#E28830",
+    paddingBottom: 5
+  },
+  buttonContainer: {
+    backgroundColor: "#E28830",
+    justifyContent: "center",
+    alignItems: "stretch",
+    marginTop: 10,
+    marginBottom: 10,
+    borderRadius: 20
+  },
+  rowView: {
+    flexDirection: "row"
+  },
+  infoTitle: {
+    color: "#E28830",
+    fontSize: 25,
+    marginTop: 5,
+    fontFamily: "TheLightFont",
+    textDecorationLine: "underline",
+    paddingBottom: 5
+  },
+  buttonTitle: {
+    color: "#2A374A",
+    fontSize: 32,
+    fontFamily: "Andy Bold",
+    marginLeft: 112
+  },
+  labelText: {
+    color: "#E28830",
+    fontSize: 20,
+    marginLeft: 15,
+    fontFamily: "typewcond_regular"
+  },
+  infoText: {
+    flex: 2,
+    color: "#E28830",
+    fontSize: 20,
+    marginLeft: 15,
+    fontFamily: "typewcond_regular",
+    flexWrap: "wrap"
   }
 };
 
-  /*render() {
-    const {
-      name,
-      completed,
-      percentage,
-      description,
-      image
-    } = this.props.selectedBeer;
-    return (
-      <View style={styles.container}>
-        <ScrollView style={{ flex: 1 }}>
-          <View style={styles.header}>
-            <Text style={styles.titleText}>{name}</Text>
-            <Image style={styles.imageStyle} source={{ uri: image }} />
-          </View>
-        </ScrollView>
-      </View>
-    );
-  }*/
-
-export default connect(mapStateToProps)(Beers);
+export default connect(mapStateToProps, { sendData, completed, updateData })(
+  Beers
+);
